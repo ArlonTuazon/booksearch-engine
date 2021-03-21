42bdec702e9ca344aecf00f5794877a97b61d2e0
ArlonTuazon <nikka1378@yahoo.com>
2021-03-21 02:51:38 -0700

    update files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -22,9 +22,43 @@ const resolvers = {
               if (!user) {
                   throw new AuthenticationError('Incorrect credentials')
               }
+
+              const correctPw = await user.isCorrectPassword(password)
+              if (!correct){
+                  throw new AuthenticationError('Incorrect Password')
+              }
               const token = signToken(user);
               return {token, user}
           },
+
+          addUser: async (parent, args) => {
+              const user = User.create(args);
+              const toker = signToken(user);
+              return {token, user};
+          },
+
+          saveBook: async (parent, {input}, context) => {
+              if (context.user) {
+                  const uodateUser = await User.findByIdAndUpdate (
+                      {_id: context.user._id},
+                      {$addToSet: {savedBooks: input}},
+                      {new:true}
+                  );
+                  return updateUser;
+              }
+              throw new AuthenticationError('You need to be logged in')
+          },
+
+          removeBook: async (parent, args, context) => {
+              if (context.user) {
+                  const updatedUSer = await User.findOneAndUpdate(
+                      {_id: context.user._id},
+                      {$pull: {savedBooks: {bookId: args.bookId}}},
+                      {new: true}
+                  );
+                  return updatedUSer;
+              }
+          }
       }
 }
 
